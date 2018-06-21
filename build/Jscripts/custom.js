5593115ee51a5e00dec5972e1570a2e20810b713
longht <longht@vng.com.vn>
2018-06-21 23:32:38 +0700

    update lv 1 ep 1

--- build/Jscripts/custom.js
+++ build/Jscripts/custom.js
@@ -1,1 +1,15 @@
-// $(function() {});
+$(function() {
+    $(".close-btn").click(function() {
+        $("#area4").hide();
+        $("#area5").hide();
+        $("body").removeClass("fixed");
+    });
+    $('.area__clickable-01 .area__button').click(function() {
+        $("body").addClass("fixed");
+        $("#area4").show();
+    });
+    $('.area__clickable-02 .area__button').click(function() {
+        $("body").addClass("fixed");
+        $("#area5").show();
+    });
+});
